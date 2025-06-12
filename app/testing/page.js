80824e49f223105d09b226a3608a6dbfdfbbb9fb
Anuj Sharma <anuj.shar2k8@gmail.com>
2025-06-12 12:50:54 +0530

    something is in progress

---
+++ app/testing/page.js
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+import Landing from '@/components/pages/Landing'
+const page = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
+
+export default page

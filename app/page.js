c74219d9c573363976fac68bb40640b12b567897
Anuj Sharma <anuj.shar2k8@gmail.com>
2025-06-15 11:47:39 +0530

    fixed

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 "use client";
-import MainLandingPage from "@/components/pages/MainLandingPage";
-import VerticalScale from "@/components/VerticalScale";
 import Landing from "@/components/pages/Landing";
 
 export default function Home() {
   return (
-    <div className="relative w-full overflow-x-hidden">
-      <MainLandingPage />
-      {/* <Landing /> */}
+    <div>
+      {/* <MainLandingPage /> */}
+      <Landing />
     </div>
   );
 }
